8b7f07b26071badabf48a1e27157ab77520bb57c
ArtemKobko <arteks505@gmail.com>
2022-07-14 21:14:58 +0200

    second commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <footer>
+     Mont Blanc &copy;
+    </footer>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function () {
+  return (
+   <header>
+      <span className='logo'>Mont Blanc</span>
+      <nav>
+        <ul className='nav'>
+          <li>About us</li>
+          <li>Contacts</li>
+          <li>User</li>
+
+        </ul>
+      </nav>
+      <div className='banner'></div>
+   </header>
+  )
+}
